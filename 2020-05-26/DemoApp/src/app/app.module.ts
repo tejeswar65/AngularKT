03bed2e211eff376ab2e8c77de06a6503ec95915
Akhi1 <Akhi1@users.noreply.github.com>
2020-05-26 13:26:11 +0530

    sample application created during today's session
    discussed about
    inline stylesheets
    class files
    one way databinding
    two way data binding
    module.ts file
    creating new component
    nesting one component inside another (root)

---
+++ 2020-05-26/DemoApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 2020-05-26/DemoApp/src/app/second/second.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Interpolation } from '@angular/compiler';
+
+@Component({
+  selector: 'app-second',
+  // template: `<p>Hello World</p>`,
+  templateUrl: './second.component.html',
+  // styles: ['p{color: #00ff00}'],
+  styleUrls: ['./second.component.css']
+})
+export class SecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  parentData: String = "Heading for parent";
+  leftChild: String = "Heading for left child";
+  rightChild: String = "Heading for right child";
+
+  isTrue = false;
+
+  isDisabled = false;
+
+  inputValue = "Hey dude how are you";
+
+  imageName1 = "kitty-cat-kitten-pet-45201.jpeg"
+  imageName2 = "kitty-cat-kitten-pet-45201.jpeg"
+
+  twoWay = ""
+
+}
